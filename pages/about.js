babdf093853571ee75dd91e1416550c083396a77
Sam Calmes <sdcalmes@gmail.com>
2017-03-28 20:06:39 -0500

    tutorial finished

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Link from 'next/link'
 import Layout from '../components/Layout.js'
 
 
 export default () => (
     <div>
         <Layout>
-            <p>Hello, Next!!!!</p>
+            <p>About page.</p>
         </Layout>
     </div>
 )
